207de5e26b7aba9182daab877c1351ca392d8130
prdgrl <91669163+prdgrl@users.noreply.github.com>
2021-11-13 15:52:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0;
+    cin>>n;
+    //cout<<n<<endl;
+    if (n>=2){
+        int i=0,b=1,a=0,c=1;
+        for(i;i<n;i++){
+            cout<<c<<endl;
+            c=a+b;
+           a=b;
+           //cout<<"a"<<a<<endl;
+           b=c;
+            //cout<<"b"<<b<<endl;
+        }}
+    else{
+        cout<<"errore"<<endl;
+    }
+    return 0;
 }
+
